2d413272582e9a5318f20e9dc5afb848f7bbfcd2
Boris Polania <boris.polania@gmail.com>
2023-03-15 09:36:58 -0700

    Fixes and Updates

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -1,14 +1,31 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./SneakerNFT.sol";
 
-contract SneakerFactory {
+contract SneakerFactory is AccessControl {
     address[] public nfts;
 
-    function createSneakerNFT() public returns (address) {
+    // Define the Admin role identifier
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    // Event emitted when a new SneakerNFT contract is created
+    event SneakerNFTCreated(address indexed nftAddress, address indexed creator);
+
+    constructor(address admin) {
+        _grantRole(ADMIN_ROLE, admin);
+    }
+
+    modifier onlyAdmin() {
+        require(hasRole(ADMIN_ROLE, msg.sender), "Caller is not a minter");
+        _;
+    }
+
+    function createSneakerNFT() public onlyAdmin returns (address) {
         SneakerNFT nft = new SneakerNFT();
         nfts.push(address(nft));
+        emit SneakerNFTCreated(address(nft), msg.sender);
         return address(nft);
     }
 
